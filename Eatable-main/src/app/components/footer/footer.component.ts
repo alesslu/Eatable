17daf04e0e03f2ef4ed4704a3e7230be92eb36df
alesslu <alessluchata@gmail.com>
2023-01-25 18:19:49 -0500

    agregando estilo a footer

--- Eatable-main/src/app/components/footer/footer.component.ts
+++ Eatable-main/src/app/components/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  styles: [`.router-link-active { background-color: red; }`],
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  
 
 }
