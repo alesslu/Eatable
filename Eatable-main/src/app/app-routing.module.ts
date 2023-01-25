034965f0df74655ce2f6e31c8acc07927164042d
alesslu <alessluchata@gmail.com>
2023-01-25 17:19:31 -0500

    agregando pagina principal y footer

--- Eatable-main/src/app/app-routing.module.ts
+++ Eatable-main/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoryComponent } from './pages/history/history.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'profile',component: ProfileComponent
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'history', component: HistoryComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Eatable-main/src/app/app.module.ts
+++ Eatable-main/src/app/app.module.ts
@@ -1,12 +1,21 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    HistoryComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
